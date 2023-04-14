24dafa1d4f2cb710591b6f7d2e15fe145bd4b01e
Vironika <nika.nika1711@gmail.com>
2023-04-14 03:21:26 +0300

    todo styles added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../css/TodoList.css";
 
 function TodoList() {
   const [items, setItems] = useState([]);
@@ -17,18 +18,34 @@ function TodoList() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleAddItem}>Add</button>
-      <ul>
+    <div className="todo">
+      <h2 className="todo-header">todo list</h2>
+      <div className="todo-form">
+        <div className="todo-input-box">
+          <input
+            className="todo-input"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <label className={"todo-label" + (text ? " filled" : " blank")}>
+            my plans
+          </label>
+        </div>
+        <button className="todo-btn" onClick={handleAddItem}>
+          Add
+        </button>
+      </div>
+      <ul className="todo-list">
         {items.map((item, index) => (
-          <li key={index}>
+          <li className="todo-list__item" key={index}>
             {item}
-            <button onClick={() => handleDeleteItem(index)}>Delete</button>
+            <button
+              className="todo-btn"
+              onClick={() => handleDeleteItem(index)}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
